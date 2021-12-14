5d98bdc58f7e467b8cd6ac4c53e4bbba754c257d
olivierdoornink <olivier@poetfarmer.com>
2021-12-14 17:01:31 +0100

    v1

---
+++ script2.js
@@ -0,0 +1,89 @@
+// NAV DROPDOWN
+
+var deButton = document.querySelector("div:nth-of-type(1)");
+
+function doeFormNeerEnOp(){
+	let hetFormulier = document.querySelector("nav");
+	hetFormulier.classList.toggle("toonForm");
+}
+
+deButton.addEventListener("click", doeFormNeerEnOp);
+
+
+function doeCtaNeerEnOp(){
+	let hetFormulier = document.querySelector("div:nth-of-type(1)");
+	deButton.classList.toggle("toonCta");
+}
+
+deButton.addEventListener("click", doeCtaNeerEnOp);
+
+
+
+// Muziek Speelt Af
+
+	var audio = new Audio ('./audio/giannis.mp3');
+	var count	= 0;
+	var muziekBtn = document.querySelector("body.highlights section:nth-of-type(1) button")
+
+	function speelMuziek(){
+		if(count==0){
+			count = 1;
+			audio.play();
+	}
+		else{
+			count = 0;
+			audio.pause();
+		}
+	}
+
+	muziekBtn.addEventListener("click", speelMuziek);
+
+
+// Button Speelt Video Af
+
+var dunkBtn = document.querySelector("#dunkbutton");
+
+function toggleDunkBtn(){
+	let video = document.querySelector("#video");
+	let text = document.querySelector("body.highlights section:nth-of-type(2) div:nth-of-type(1) p");
+	video.src= "https://www.youtube.com/embed/0USzJs9agOk?autoplay=1&controls=0&mute=1";
+	text.innerHTML = "Watch a video of Giannis' best dunks!";
+}
+
+dunkBtn.addEventListener("click", toggleDunkBtn);
+
+
+var shootBtn = document.querySelector("#shootbutton");
+
+function toggleShootBtn(){
+	let video = document.querySelector("#video");
+	let text = document.querySelector("body.highlights section:nth-of-type(2) div:nth-of-type(1) p");
+	video.src= "https://www.youtube.com/embed/IuodljGJnfM?autoplay=1&start=20&mute=1&controls=0";
+	text.innerHTML = "Watch a video of Giannis' greatest jumpshots!";
+}
+
+shootBtn.addEventListener("click", toggleShootBtn);
+
+
+var playBtn = document.querySelector("#playbutton");
+
+function togglePlayBtn(){
+	let video = document.querySelector("#video");
+	let text = document.querySelector("body.highlights section:nth-of-type(2) div:nth-of-type(1) p");
+	video.src= "https://www.youtube.com/embed/UeQhz_oSquk?autoplay=1&start=20&mute=1&controls=0";
+	text.innerHTML = "Watch a video of Giannis' insane playmaking skills!";
+}
+
+playBtn.addEventListener("click", togglePlayBtn);
+
+
+var defenceBtn = document.querySelector("#defencebutton");
+
+function toggleDefenceBtn(){
+	let video = document.querySelector("#video");
+	let text = document.querySelector("body.highlights section:nth-of-type(2) div:nth-of-type(1) p");
+	video.src= "https://www.youtube.com/embed/QbNpDuwiaMA?autoplay=1&start=20&mute=1&controls=0";
+	text.innerHTML = "Watch a video of Giannis' unreal defence!";
+}
+
+defenceBtn.addEventListener("click", toggleDefenceBtn);
